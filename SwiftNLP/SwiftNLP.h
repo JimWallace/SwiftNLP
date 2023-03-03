64a12cc4bd8d2d6937e5994fcdf4990c0a63fbe0
Jim Wallace <james.wallace@uwaterloo.ca>
2023-03-02 20:56:48 -0500

    Initial Commit

---
+++ SwiftNLP/SwiftNLP.h
@@ -0,0 +1,18 @@
+//
+//  SwiftNLP.h
+//  SwiftNLP
+//
+//  Created by Jim Wallace on 2023-03-02.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftNLP.
+FOUNDATION_EXPORT double SwiftNLPVersionNumber;
+
+//! Project version string for SwiftNLP.
+FOUNDATION_EXPORT const unsigned char SwiftNLPVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftNLP/PublicHeader.h>
+
+
